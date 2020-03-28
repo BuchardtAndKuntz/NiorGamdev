0b6de3ed7c24fec450689b17be5f7bac13c2816b
Christopher <christopher.buchardt@gmail.com>
2020-03-28 22:26:49 +0100

    Added a wincauldron

---
+++ Scenes/PickupScreens/WinScreen.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+onready var label = $Label
+var StrSttart = "Congratulations you made it home to your cauldron! \n Time to for some poultry soup! \n \n \n \n You found "
+var StrEnd = " ! \n \n Press escape to quit \n or \n press Enter to keep playing"
+
+
+
+func updateCount():
+	label.text = String(StrSttart + str(AbilityFlags.ingredients) + StrEnd)
+
+
+func _on_WinScreen_visibility_changed():
+	if visible==true:
+		updateCount()
