5a7ed2357317924132870db92cccb6201c7d2838
Christopher <christopher.buchardt@gmail.com>
2020-03-28 13:37:41 +0100

    Added other abilities to pickup

--- GameObjects/PickUp.gd
+++ GameObjects/PickUp.gd
@@ -3,8 +3,18 @@ extends Area2D
 
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
-	print(bodies)
 	for body in bodies:
 		if body.get_name() == "Player":
-			AbilityFlags.pickedUpOwl = true
-			get_parent().remove_child(self)
+			match name:
+				"Glide":
+					AbilityFlags.pickedUpGlide = true
+					get_parent().remove_child(self)
+				"Owl":
+					AbilityFlags.pickedUpOwl = true
+					get_parent().remove_child(self)
+				"DoubleJump":
+					AbilityFlags.pickedUpJump = true
+					get_parent().remove_child(self)
+				"Swap":
+					AbilityFlags.pickedUpSwap = true
+					get_parent().remove_child(self)
--- Scripts/AbilityFlags.gd
+++ Scripts/AbilityFlags.gd
@@ -7,6 +7,8 @@ var hasGlide = false
 var hasOwlSwitch = false
 var canThrowOwl = false
 
+var pickedUpGlide = false
+var pickedUpJump = false
+var pickedUpSwap = false
 var pickedUpOwl = false
 var movementAllowed = true
-
